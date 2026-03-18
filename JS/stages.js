1a94f3a85ff9daf3c8470a8fc17c6afaf615ab75
Rafael <redpt3219@gmail.com>
2026-03-18 13:03:02 +0100

    encore d'édits

--- JS/stages.js
+++ JS/stages.js
@@ -2,16 +2,14 @@ const stages = {
     accessCreed: {
         title: "Access Creed",
         html: `
-            <div class="stage" id="access-creed">
+            <div class="stage" id="Access-creed">
                 <div class="container-img">
-                    <img src="CSS/IMG/accesscreed.png" alt="Access Creed" class="img-centre2">
+                    <img src="CSS/IMG/accesscreed.png" alt="Access Creed" class="img-centre">
                 </div>
-                <ul>
-                    <li>Une entreprise Freelancer</li>
-                    <li>Aide à réparer les soucis des ordinateurs de ses clients (dans d’autres entreprises) via du code PowerShell</li>
-                    <li>Fait des délivrances d’outils (ordinateurs, composants, écrans, etc.).</li>
-                    <li>Fait des applications web pour les clients.</li>
-                </ul>
+                <p>Une entreprise Freelancer créée en 2018</p>
+                <p>Aide à réparer les soucis des ordinateurs de ses clients (dans d’autres entreprises) via du code PowerShell</p>
+                <p>Fait des délivrances d’outils (ordinateurs, composants, écrans, etc.).</p>
+                <p>Fait des applications web pour les clients.</p>
                 
                 <h2>J'ai fait quoi dans Access Creed ?</h2>
                 <p>Du 19 mai 2025 au 27 juin 2025, j'ai eu un projet et une mission :</p>
@@ -20,7 +18,7 @@ const stages = {
                     <li>Ma Mission : un formulaire applicatif en C# pour une autre entreprise.</li>
                 </ul>
 
-                <h2>Le Projet du site web</h2>
+                <h2>Site Web</h2>
                 <p>Ce projet est un site web en HTML/CSS/JS et PHP natif.</p>
                 <p>Le but du projet est de faire un site web pour que les clients aient un moyen de contacter l'entreprise.</p>
                 <p>Et pour qu'ils aient un une compte sécurisé grâce aux tickets.</p>
@@ -37,7 +35,7 @@ const stages = {
                         <img id="stage" src="CSS/IMG/access-creed-4.png" alt="AC4" class="img-centre2 img-lightbox" onclick="openLightbox(this.src)">
                     </div>
                 </div>
-                <h2>La Mission du Formulaire en C#</h2>
+                <h2>Formulaire sous C#</h2>
                 <p>Cette mission est pour faire un formulaire sous C#.</p>
                 <p>Elle est mise en forme par XAML et après transformée en fichier exécutable.</p>
                 <p>Le but c'est qu'on obtient de l'information des ordinateurs et lesquels les utilisent pour une entreprise.</p>
@@ -52,16 +50,15 @@ const stages = {
     celeonet: {
         title: "Celeonet",
         html: `
-            <div class="stage" id="celeonet">
+            <div class="stage" id="Celeonet">
                 <div class="container-img">
                     <img src="CSS/IMG/celeonet.png" alt="Celeonet" class="img-centre2">
                 </div>
-                <ul>
-                    <li>Une entreprise experte en hébergement créée en 2003.</li>
-                    <li>L'entreprise est constituée d'une équipe d’experts d’internet et des réseaux.</li>
-                    <li>C'est une entreprise qui se répartit sur la région parisienne et lyonnaise.</li>
-                    <li>Pour en savoir plus,<a href="https://www.celeonet.fr/societe" target="_blank">cliquer ici.</a></li>
-                </ul>
+                <p>Une entreprise experte en hébergement créée en 2003.</p>
+                <p>L'entreprise est constituée d'une équipe d’experts d’internet et des réseaux.</p>
+                <p>C'est une entreprise qui se répartit sur la région parisienne et lyonnaise.</p>
+                <p>Pour en savoir plus, <a href="https://www.celeonet.fr/societe" target="_blank">cliquer ici.</a></p>
+                
 
                 <h2>J'ai fait quoi dans Celeonet ?</h2>
                 <p>Du 5 janvier 2026 au 13 Février 2026, J'ai eu un projet et deux missions :</p>
@@ -71,7 +68,7 @@ const stages = {
                     <li>Ma Deuxième Mission : mise en place des Test Unitaires en PHP.</li>
                 </ul>
 
-                <h2>L'application web</h2>
+                <h2>Application Web</h2>
                 <p>Ce projet est une application web en HTML/CSS, JS et PHP natif.</p>
                 <p>Le but du projet est d'utiliser la BDD Asterisk pour obtenir des informations.</p>
                 <p>Et grâce aux tables de cette BDD, on peut faire des requêtes MySQL pour obtenir :</p>
@@ -95,14 +92,14 @@ const stages = {
                 </div>
                 <p>Pour une petite info, la synthèse vient d'une autre BDD nommée CallSupport.</p>
 
-                <h2>Les Test Unitaires en PHP</h2>
+                <h2>Test Unitaires</h2>
                 <p>Cette mission est pour faire des test unitaires sous PHP.</p>
                 <p>Elle est mise en forme par le framework PHPUnit, et grâce à ça,</p>
                 <p>On peut faire tester les fonctions faites pour voir si elles marchent vraiment.</p>
                 <h3>Voici le test fait directement en terminal :</h3>
                 <img id="stage" src="CSS/IMG/celeonet-phpunit.png" alt="CeleonetPHPUnit" class="img-centre2 img-lightbox" onclick="openLightbox(this.src)">
 
-                <h2>L'API</h2>
+                <h2>API</h2>
                 <p>Cette mission est pour faire une API sur les mêmes BDD.</p>
                 <p>Elle est mise en forme par le framework Symfony, et grâce à ça,</p>
                 <p>On peut faire des requêtes GET.</p>
